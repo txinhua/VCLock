03f65c965dbeebbe7d9a7d231bff572a502b214b
gftang <gftang@vcainfo.com>
2016-10-09 17:14:57 +0800

    first commit

---
+++ PassWordLockDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PassWordLockDemo
+//
+//  Created by VcaiTech on 16/9/25.
+//  Copyright © 2016年 Tang guifu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
